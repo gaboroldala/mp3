dfc8a7ee49608e0109e8e227a1698d134111c319
Gabor Oldala <137519692+gaboroldala@users.noreply.github.com>
2023-06-29 19:39:29 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,48 +1,23 @@
 <?php
-if (isset($_GET['link']) && isset($_GET['type']) && isset($_GET['quality'])) {
-    $youtubeLink = $_GET['link'];
-    $mediaType = $_GET['type'];
-    $quality = $_GET['quality'];
+if (isset($_POST['download'])) {
+    $youtubeLink = $_POST['youtubeLink'];
+    $fileType = $_POST['fileType'];
+    $quality = $_POST['quality'];
 
-    // Implementáld a letöltési logikát itt
-    // Például használhatsz YouTube letöltési könyvtárakat vagy API-kat
-    // és a kiválasztott minőség alapján állítsd elő a letöltési fájlt
-
-    // Példa letöltési folyamat
-    if ($mediaType === 'audio') {
-        // Letöltési folyamat az MP3 esetén
-        $videoId = getVideoId($youtubeLink); // YouTube videó azonosító kinyerése a linkből
-        $fileName = getVideoTitle($videoId) . '.mp3'; // Zene címének használata a letöltött fájl nevében
-
-        // Példa: letöltési fájl letöltése a böngészőbe
-        header('Content-Type: audio/mpeg');
-        header('Content-Disposition: attachment; filename="' . $fileName . '"');
-        readfile($filePath); // $filePath az MP3 fájl teljes elérési útja
+    // Letöltési folyamat
+    if ($fileType === 'audio') {
+        $command = 'youtube-dl -x --audio-format mp3 --audio-quality ' . $quality . ' --output "%(title)s.%(ext)s" ' . escapeshellarg($youtubeLink);
     } else {
-        // Letöltési folyamat az MP4 esetén
-        $videoId = getVideoId($youtubeLink); // YouTube videó azonosító kinyerése a linkből
-        $fileName = getVideoTitle($videoId) . '.mp4'; // Videó címének használata a letöltött fájl nevében
-
-        // Példa: letöltési fájl letöltése a böngészőbe
-        header('Content-Type: video/mp4');
-        header('Content-Disposition: attachment; filename="' . $fileName . '"');
-        readfile($filePath); // $filePath az MP4 fájl teljes elérési útja
+        $command = 'youtube-dl -f "bestvideo[height<=' . $quality . ']+bestaudio[ext=m4a]/best[ext=mp4]" --output "%(title)s.%(ext)s" ' . escapeshellarg($youtubeLink);
     }
-}
 
-function getVideoId($youtubeLink) {
-    // Implementáld a YouTube videó azonosítójának kinyerését a linkből
-    // Példa: https://www.youtube.com/watch?v=VIDEO_ID
-    $parts = parse_url($youtubeLink);
-    parse_str($parts['query'], $query);
-    $videoId = $query['v'];
-    return $videoId;
-}
+    // Parancs végrehajtása
+    exec($command, $output, $return_var);
 
-function getVideoTitle($videoId) {
-    // Implementáld a YouTube videó címének lekérését a videó azonosító alapján
-    // Példa: Használhatsz YouTube API-t vagy web scraping technikát
-    // Visszatérítsd a videó címét
-    return "Video címe"; // Példa: "Video címe"
+    if ($return_var === 0) {
+        echo 'A letöltés sikeresen megtörtént.';
+    } else {
+        echo 'Hiba történt a letöltés során.';
+    }
 }
 ?>
