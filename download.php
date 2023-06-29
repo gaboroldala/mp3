09486e5d8aca4f93d651101c08a8296a7f4eb135
Gabor Oldala <137519692+gaboroldala@users.noreply.github.com>
2023-06-29 19:26:40 +0200

    Update download.php

--- download.php
+++ download.php
@@ -11,21 +11,38 @@ if (isset($_GET['link']) && isset($_GET['type']) && isset($_GET['quality'])) {
     // Példa letöltési folyamat
     if ($mediaType === 'audio') {
         // Letöltési folyamat az MP3 esetén
-        // ...
-        // Példa: letöltési fájl nevének beállítása
-        $fileName = 'audio.mp3';
+        $videoId = getVideoId($youtubeLink); // YouTube videó azonosító kinyerése a linkből
+        $fileName = getVideoTitle($videoId) . '.mp3'; // Zene címének használata a letöltött fájl nevében
+
         // Példa: letöltési fájl letöltése a böngészőbe
-        header('Content-Type: application/octet-stream');
+        header('Content-Type: audio/mpeg');
         header('Content-Disposition: attachment; filename="' . $fileName . '"');
         readfile($filePath); // $filePath az MP3 fájl teljes elérési útja
     } else {
         // Letöltési folyamat az MP4 esetén
-        // ...
-        // Példa: letöltési fájl nevének beállítása
-        $fileName = 'video.mp4';
+        $videoId = getVideoId($youtubeLink); // YouTube videó azonosító kinyerése a linkből
+        $fileName = getVideoTitle($videoId) . '.mp4'; // Videó címének használata a letöltött fájl nevében
+
         // Példa: letöltési fájl letöltése a böngészőbe
-        header('Content-Type: application/octet-stream');
+        header('Content-Type: video/mp4');
         header('Content-Disposition: attachment; filename="' . $fileName . '"');
         readfile($filePath); // $filePath az MP4 fájl teljes elérési útja
     }
 }
+
+function getVideoId($youtubeLink) {
+    // Implementáld a YouTube videó azonosítójának kinyerését a linkből
+    // Példa: https://www.youtube.com/watch?v=VIDEO_ID
+    $parts = parse_url($youtubeLink);
+    parse_str($parts['query'], $query);
+    $videoId = $query['v'];
+    return $videoId;
+}
+
+function getVideoTitle($videoId) {
+    // Implementáld a YouTube videó címének lekérését a videó azonosító alapján
+    // Példa: Használhatsz YouTube API-t vagy web scraping technikát
+    // Visszatérítsd a videó címét
+    return "Video címe"; // Példa: "Video címe"
+}
+?>
