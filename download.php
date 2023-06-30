27dfb67a62f8a3db5ee65fc506a4a0b9a0306477
Gabor Oldala <137519692+gaboroldala@users.noreply.github.com>
2023-06-30 16:53:50 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,65 +1,23 @@
 <?php
-if (isset($_POST['download'])) {
-    $youtubeLink = $_POST['youtubeLink'];
-    $fileType = $_POST['fileType'];
-    $quality = $_POST['quality'];
+if (isset($_GET['youtubeLink']) && isset($_GET['fileType']) && isset($_GET['quality'])) {
+  $youtubeLink = $_GET['youtubeLink'];
+  $fileType = $_GET['fileType'];
+  $quality = $_GET['quality'];
 
-    // YouTube videó információ lekérése
-    $videoId = getVideoId($youtubeLink);
-    $videoInfo = getVideoInfo($videoId);
+  // Letöltési folyamat
+  if ($fileType === 'audio') {
+    $command = 'youtube-dl -x --audio-format mp3 --audio-quality ' . $quality . ' ' . escapeshellarg($youtubeLink);
+  } else {
+    $command = 'youtube-dl -f bestvideo[height<=' . $quality . ']+' . 'bestaudio/best' . ' ' . escapeshellarg($youtubeLink);
+  }
 
-    if ($videoInfo) {
-        $title = $videoInfo['title'];
-        $extension = ($fileType === 'audio') ? 'mp3' : 'mp4';
-        $filename = sanitizeFilename($title) . '.' . $extension;
+  // Parancs végrehajtása
+  exec($command, $output, $return_var);
 
-        // Letöltési folyamat
-        $downloadUrl = ($fileType === 'audio') ? $videoInfo['audioUrl'] : $videoInfo['videoUrl'];
-        downloadFile($downloadUrl, $filename);
-    }
-}
-
-// YouTube videó ID kinyerése a linkből
-function getVideoId($link) {
-    $parsedUrl = parse_url($link);
-    parse_str($parsedUrl['query'], $queryParams);
-    if (isset($queryParams['v'])) {
-        return $queryParams['v'];
-    } elseif (isset($queryParams['watch?v'])) {
-        return $queryParams['watch?v'];
-    }
-    return false;
-}
-
-// YouTube videó információ lekérése a YouTube API-val
-function getVideoInfo($videoId) {
-    $apiKey = 'AIzaSyD8DRjDK1UMDiKmwPMygljSswQjVZgBPvw'; // Helyettesítsd be a saját YouTube API kulcsoddal
-    $url = 'https://www.googleapis.com/youtube/v3/videos?id=' . $videoId . '&key=' . $apiKey . '&part=snippet';
-    $response = file_get_contents($url);
-    $data = json_decode($response, true);
-
-    if (isset($data['items'][0])) {
-        $title = $data['items'][0]['snippet']['title'];
-        $audioUrl = 'https://www.youtube.com/watch?v=' . $videoId;
-        $videoUrl = 'https://www.youtube.com/watch?v=' . $videoId;
-        return array('title' => $title, 'audioUrl' => $audioUrl, 'videoUrl' => $videoUrl);
-    }
-    return false;
-}
-
-// Biztonságos fájlnév létrehozása
-function sanitizeFilename($filename) {
-    $filename = preg_replace('/[^a-zA-Z0-9\s\-_.]/', '', $filename);
-    $filename = str_replace(' ', '_', $filename);
-    return $filename;
-}
-
-// Fájl letöltése
-function downloadFile($url, $filename) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="' . $filename . '"');
-    readfile($url);
-    exit;
+  if ($return_var === 0) {
+    echo 'A letöltés sikeresen megtörtént.';
+  } else {
+    echo 'Hiba történt a letöltés során.';
+  }
 }
 ?>
